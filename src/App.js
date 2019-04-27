575f4bbd1298ddfe6b1484f7baec393cf52a887d
oehc123 <josesvz@gmail.com>
2019-04-27 16:26:13 -0400

    backCard First Commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import cardList from './card-list'
 // import logo from "./logo.svg";
 import './App.css'
 import Card from './components/card'
+
+console.log(cardList[0])
+const cardTest = cardList.paths[0]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>SparkPath</h1>
-        <Card />
+        <Card cardTest={cardTest} />
       </header>
     </div>
   )
--- src/components/card.js
+++ src/components/card.js
@@ -2,37 +2,76 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
+  padding: 25px;
   display: flex;
-  background: red;
+  background: rgb(96, 183, 208);
+  align: center;
+  flex-direction: column;
 `
 
 const Blurb = styled.div`
+  margin-top: 25px;
   display: flex;
+  text-align: left;
+  font-size: 10px;
 `
-const Careers = styled.div`
+const Careers = styled.ul`
+  margin-top: 25px;
+  font-size: 15px;
+  background: white;
+  color: black;
   display: flex;
 `
 const Title = styled.div`
-  color: white;
-  text-transform: uppercase;
+  margin-top: 25px;
+  text-align: left;
   font-size: 1.4rem;
+`
+
+const DataCornerLowLeft = styled.div`
+  font-size: 15px;
+  margin-left: 1px;
+`
+
+const DataCornerLowRight = styled.div`
+  font-size: 15px;
+  margin-right: 1px;
+`
+
+const Footer = styled.div`
+  background-color: black;
+  justify-content: space-between;
+  margin-bottom: 1px;
+  display: flex;
 `
 
 const Back = ({ en }) => {
   const { title, blurb_1, blurb_2, careers, variant } = en
-  console.log(title, en)
   return (
     <Wrapper style={{ background: variant }}>
       <Title>{title}</Title>
+
       <Blurb>{blurb_1}</Blurb>
-      <Careers>{careers}</Careers>
+
+      <Careers>
+        {careers.map(career => (
+          <li>{career}</li>
+        ))}
+      </Careers>
+
       <Blurb>{blurb_2}</Blurb>
+
+      <Footer>
+        <DataCornerLowLeft> mysparkpath.com </DataCornerLowLeft>
+        <DataCornerLowRight> LOADLOGOHERE</DataCornerLowRight>
+      </Footer>
     </Wrapper>
   )
 }
 
-const Card = () => {
-  return <Back en={{ title: 'TITLEJOSE' }} />
+const Card = currentCard => {
+  console.log(currentCard)
+  return <Back en={currentCard.cardTest.en} />
 }
 
 export default Card
