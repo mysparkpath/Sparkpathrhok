e6abf28481805d18e325147c6e6d314fa7f5de9c
Mat Dupont <dupont.mat.l@gmail.com>
2019-04-27 13:07:42 -0400

    added support to sort cards into piles as well as a single undo

---
+++ src/state.js
@@ -0,0 +1,19 @@
+export const initialDeckState = {
+  initial: [
+    {
+      key: "health-1",
+      name: "redesign the health care system"
+    },
+    {
+      key: "protect-1",
+      name: "protect society from crime"
+    },
+    {
+      key: "food-1",
+      name: "create new food and food systems"
+    }
+  ],
+  no: [],
+  maybe: [],
+  yes: []
+};
