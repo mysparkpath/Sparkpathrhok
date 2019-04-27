ca51418ab51996a7112465ab2e8ee6b3a793fd25
oehc123 <josesvz@gmail.com>
2019-04-27 13:18:23 -0400

    firstBackCard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 // import logo from "./logo.svg";
 import './App.css'
+import Card from './components/card'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>SparkPath</h1>
+        <Card />
       </header>
     </div>
   )
--- src/components/card.js
+++ src/components/card.js
@@ -1,7 +1,38 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  display: flex;
+  background: red;
+`
+
+const Blurb = styled.div`
+  display: flex;
+`
+const Careers = styled.div`
+  display: flex;
+`
+const Title = styled.div`
+  color: white;
+  text-transform: uppercase;
+  font-size: 1.4rem;
+`
+
+const Back = ({ en }) => {
+  const { title, blurb_1, blurb_2, careers, variant } = en
+  console.log(title, en)
+  return (
+    <Wrapper style={{ background: variant }}>
+      <Title>{title}</Title>
+      <Blurb>{blurb_1}</Blurb>
+      <Careers>{careers}</Careers>
+      <Blurb>{blurb_2}</Blurb>
+    </Wrapper>
+  )
+}
 
 const Card = () => {
-  return <div />
+  return <Back en={{ title: 'TITLEJOSE' }} />
 }
 
 export default Card
