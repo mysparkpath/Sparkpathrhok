5c46c03545da79ec1e6edd8cc5ade2d5cf98350e
Jason Guo <jasonyzguo@gmail.com>
2019-07-14 23:55:32 -0400

    add welcome text to user

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Box, Text, Navbar, Link } from '../components'
 import Firebase from 'firebase'
 import { firebaseConfig } from './config'
 
 Firebase.initializeApp(firebaseConfig)
 
-const Login = () => {
+const auth = setUser => {
   const provider = new Firebase.auth.GoogleAuthProvider()
   // TODO: Add en/fr language choice
   // Firebase.auth().languageCode =
@@ -17,13 +17,31 @@ const Login = () => {
       // Google Access Token that lets you access Google API
       const token = result.credential.accessToken
       const user = result.user
-      console.log(user)
+      setUser(user)
     })
     .catch(err => console.error(err))
+}
 
+const Login = () => {
+  const [user, setUser] = useState()
+  useEffect(() => auth(setUser), [])
+  console.log(user)
   return (
     <Box bg="purple" flexDirection="column" color="white" flex="1">
       <Navbar bg="purple" />
+      <Box flexDirection="column" alignItems="center" mx={6}>
+        {user && (
+          <Text
+            textAlign="center"
+            fontSize={5}
+            fontWeight="600"
+            lineHeight="1.3"
+            maxWidth="50rem"
+          >
+            Welcome {user.displayName} to the Digital Challenge Cards!
+          </Text>
+        )}
+      </Box>
     </Box>
   )
 }
