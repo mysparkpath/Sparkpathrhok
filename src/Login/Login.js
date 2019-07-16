187fde1b640e7c759381406bc6b3f01ae39472d7
Jason Guo <jasonyzguo@gmail.com>
2019-07-16 00:24:25 -0400

    add save cards functionality

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -35,6 +35,7 @@ const signIn = setUser => {
       // Google Access Token that lets you access Google API
       const token = result.credential.accessToken
       const user = result.user
+
       setUser(user)
     })
     .catch(err => console.error(err))
@@ -48,25 +49,72 @@ const callDatabase = async ({ email, displayName, uid: userId }, setUser) => {
     console.log(
       await new Promise((res, rej) => {
         database.ref('users/' + userId).set({
-          name: displayName,
+          displayName: displayName,
           email: email,
+          uid: userId,
         })
         res('Successfully added to database')
         rej('Failed to add to database')
       })
     )
-  // else setUser(user)
+  else return setUser({ ...user, saved: true })
 }
 
 const handleSave = async ({ uid: userId }, myTop3) => {
+  console.log(userId)
   await new Promise((res, rej) => {
     database.ref('users/' + userId).update({
-      myTop3: { 123: 4 },
+      myTop3: myTop3,
     })
     console.log('Successfully added to database')
     res('Successfully added to database')
     rej('Failed to add to database')
   })
+}
+const renderCards = (myTop3, lang) => {
+  return (
+    myTop3.length > 0 &&
+    myTop3.map(({ image_path, altText, variant, en, fr, key }) => {
+      const path = require(`../${image_path}`)
+
+      const title = lang === 'en' ? en.title : fr.title
+
+      return (
+        <Box
+          key={key}
+          mt="2rem"
+          justifyContent="flex-start"
+          alignItems="center"
+        >
+          <Box
+            height="6rem"
+            width="6rem"
+            mr="3rem"
+            bg={variant}
+            justifyContent="center"
+            borderRadius="4px"
+            alignItems="center"
+            flex="1 0 auto"
+          >
+            <img
+              style={{ height: '5rem', width: '5rem' }}
+              src={path}
+              alt={altText}
+            />
+          </Box>
+
+          <Text
+            color="rgb(255, 255, 255)"
+            textTransform="capitalize"
+            fontSize="1.8rem"
+            textAlign="left"
+          >
+            {title}
+          </Text>
+        </Box>
+      )
+    })
+  )
 }
 
 const Login = () => {
@@ -76,8 +124,11 @@ const Login = () => {
   useEffect(() => {
     signIn(setUser)
   }, [])
-  if (user) callDatabase(user, setUser)
-  console.log(user)
+  // TODO: Refactor the return values into functions
+  if (user) {
+    if (user.saved !== true) callDatabase(user, setUser)
+  }
+  if (user) console.log(user.myTop3)
   if (user)
     return (
       <Box bg="purple" flexDirection="column" color="white" flex="1">
@@ -95,18 +146,20 @@ const Login = () => {
               Welcome to the Digital Challenge Cards {user.displayName}!
             </Text>
           }
-          {myTop3.length > 0 && (
-            <Text
-              textAlign="center"
-              fontSize={5}
-              fontWeight="600"
-              lineHeight="1.3"
-              maxWidth="50rem"
-              style={{ 'padding-top': '5rem' }}
-            >
-              Your challenge cards are:
-            </Text>
-          )}
+
+          <Text
+            textAlign="center"
+            fontSize={5}
+            fontWeight="600"
+            lineHeight="1.3"
+            maxWidth="50rem"
+            style={{ 'padding-top': '5rem' }}
+          >
+            {user.myTop3
+              ? 'Your saved challenge cards are:'
+              : myTop3.length > 0 && 'Your challenge cards are:'}
+          </Text>
+
           <Box
             mt="2rem"
             flexDirection="column"
@@ -114,47 +167,7 @@ const Login = () => {
             color="black"
             px="3rem"
           >
-            {myTop3.length > 0 &&
-              myTop3.map(({ image_path, altText, variant, en, fr, key }) => {
-                const path = require(`../${image_path}`)
-
-                const title = lang === 'en' ? en.title : fr.title
-
-                return (
-                  <Box
-                    key={key}
-                    mt="2rem"
-                    justifyContent="flex-start"
-                    alignItems="center"
-                  >
-                    <Box
-                      height="6rem"
-                      width="6rem"
-                      mr="3rem"
-                      bg={variant}
-                      justifyContent="center"
-                      borderRadius="4px"
-                      alignItems="center"
-                      flex="1 0 auto"
-                    >
-                      <img
-                        style={{ height: '5rem', width: '5rem' }}
-                        src={path}
-                        alt={altText}
-                      />
-                    </Box>
-
-                    <Text
-                      color="rgb(255, 255, 255)"
-                      textTransform="capitalize"
-                      fontSize="1.8rem"
-                      textAlign="left"
-                    >
-                      {title}
-                    </Text>
-                  </Box>
-                )
-              })}
+            {user.myTop3 ? renderCards(user.myTop3, lang) : renderCards(myTop3)}
           </Box>
         </Box>
         {myTop3.length > 0 && (
