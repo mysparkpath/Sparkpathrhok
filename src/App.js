d3f0d3991d89b346139e3be90635d39d5544ced4
Jason Guo <jasonyzguo@gmail.com>
2019-07-19 00:34:17 -0400

    add route to home page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from "react";
-import { GlobalStyle } from "./styles";
-import "./App.css";
-import { Router } from "@reach/router";
-import Layout from "./Layout";
-import Main from "./Main";
-import CardApp from "./CardApp";
-import CardSelector from "./CardApp/CardSelector";
-import WellDone from "./CardApp/WellDone";
-import GetStarted from "./get-started";
-import Instructions from "./instructions";
-import Login from "./Login/Login";
-import { ThemeProvider } from "styled-components";
-import { LanguageProvider, DeckProvider, Top3Provider } from "./state";
-import { theme } from "./components";
+import React from 'react'
+import { GlobalStyle } from './styles'
+import './App.css'
+import { Router } from '@reach/router'
+import Layout from './Layout'
+import Main from './Main'
+import CardApp from './CardApp'
+import CardSelector from './CardApp/CardSelector'
+import WellDone from './CardApp/WellDone'
+import GetStarted from './get-started'
+import Instructions from './instructions'
+import Login from './Login/Login'
+import Home from './Home/Home'
+import { ThemeProvider } from 'styled-components'
+import {
+  LanguageProvider,
+  DeckProvider,
+  Top3Provider,
+  UserProvider,
+} from './state'
+import { theme } from './components'
 
 const App = () => {
   return (
@@ -20,25 +26,28 @@ const App = () => {
       <LanguageProvider>
         <DeckProvider>
           <Top3Provider>
-            <Layout>
-              <GlobalStyle />
-              <Router style={{ flex: 1, display: "flex" }}>
-                <Main path="/" />
-                <GetStarted path="/get-started" />
-                <Instructions path="/instructions" />
+            <UserProvider>
+              <Layout>
+                <GlobalStyle />
+                <Router style={{ flex: 1, display: 'flex' }}>
+                  <Main path="/" />
+                  <GetStarted path="/get-started" />
+                  <Instructions path="/instructions" />
 
-                <CardApp path="/cards" />
-                <CardSelector path="/top3" />
+                  <CardApp path="/cards" />
+                  <CardSelector path="/top3" />
 
-                <WellDone path="/welldone" />
-                <Login path="/login" />
-              </Router>
-            </Layout>
+                  <WellDone path="/welldone" />
+                  <Login path="/login" />
+                  <Home path="/home" />
+                </Router>
+              </Layout>
+            </UserProvider>
           </Top3Provider>
         </DeckProvider>
       </LanguageProvider>
     </ThemeProvider>
-  );
-};
+  )
+}
 
-export default App;
+export default App
